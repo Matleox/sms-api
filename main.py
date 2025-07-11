6d29576ce88632f26d3f5c911bc0862781b7560f
Matleox <68650899+Matleox@users.noreply.github.com>
2025-07-11 04:02:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -186,3 +186,8 @@ async def add_key(data: dict, token: str = Depends(oauth2_scheme), db: SessionLo
     })
     db.commit()
     return {"status": "success", "message": f"Key {key} eklendi!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
